1523ef63371c529b7b99f3dbb930656fa5f6f823
Naveen R <35594720+naveenrj98@users.noreply.github.com>
2019-11-11 11:42:42 +0530

    #--------------This File Belongs to Naveen RJ-(1bm16CS055)----------------

--- 3a.R
+++ 3a.R
@@ -1,3 +1,4 @@
+#--------------This File Belongs to Naveen RJ-(1bm16CS055)----------------
 matrixbee=matrix(data=c(10,1,37,5,12,8,3,9,6,4,18,9,12,4,6,8,27,6,32,23,12,13,16,9,10),nrow=5,ncol=5)
 matrixbee
 plantnames=list("Thistle","Vipers","GoldenRain","Yellowalfala","blackberry")
